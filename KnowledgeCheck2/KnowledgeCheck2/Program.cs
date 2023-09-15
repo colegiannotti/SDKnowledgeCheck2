a5a8eebf1811aff5b674b06b4084dd07b09bdeb5
cole.giannotti <colegiannotti@gmail.com>
2023-09-15 15:23:01 -0400

    Test

---
+++ KnowledgeCheck2/KnowledgeCheck2/Program.cs
@@ -0,0 +1,40 @@
+﻿using KnowledgeCheck2;
+
+Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<Warrior>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+	string? line;
+	int tryResult;
+	bool success = false;
+	// In this loop, populate the object's properties using Console.ReadLine()
+	var myClass = new Warrior();
+
+	Console.WriteLine("\nEnter the value for Name:");
+	myClass.Name = Console.ReadLine();
+
+	Console.WriteLine("Enter the value for Strength:");
+	do
+	{
+		line = Console.ReadLine();
+		success = int.TryParse(line, out tryResult);
+		if (success)
+        {
+			myClass.Strength = tryResult;
+        } else
+        {
+			Console.WriteLine("Input was not a valid number. Please try again");
+		}
+	} while (!success);
+
+
+	recordList.Add(myClass);
+}
+
+// Print out the list of records using Console.WriteLine()
+foreach (Warrior record in recordList)
+{
+	Console.WriteLine(record.ToString());
+}
---
+++ KnowledgeCheck2/KnowledgeCheck2/Warrior.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    internal class Warrior : Adventurer
+    {
+        public int Strength { get; set; }
+        public override string ToString()
+        {
+            return String.Format("Warrior {0} has {1} strength.", Name, Strength);
+        }
+    }
+
+    
+}
